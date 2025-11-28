3c30f1df3498b7bd1b298be4e74df5de0bdc29d0
jeanclaramartinez <jeanclaramartinez@gmail.com>
2025-11-27 16:28:30 -0800

    creation du fichier java Quadrilatere

---
+++ TP/src/Quadrilatere.java
@@ -0,0 +1,11 @@
+public class Quadrilatere {
+    private double cote1;
+    private double cote2;
+    private double cote3;
+    private double cote4;
+/*
+    public Quadrilatere {
+       private double
+    }
+    */
+}
